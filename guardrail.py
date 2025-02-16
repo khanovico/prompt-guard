6e06ebf3385b3bd64fea66cbb30dd943b4c3bb24
Khanovico <khanovicdev@gmail.com>
2025-02-16 02:39:03 -0300

    feat: enhance Guardrail class with MongoDB and FAISS support for vector similarity search

--- guardrail.py
+++ guardrail.py
@@ -1,49 +1,31 @@
-import concurrent.futures
 import logging
-
-from huggingface import Embedding
-from providers import VectorStore, Sanitize, AnomalyDetection
-
-from pydantic import BaseModel
-from enum import Enum
-import numpy as np
-
-import torch.nn.functional as F
-import math
-
-from collections import Counter
+import concurrent.futures
 import nltk
+import pymongo
+import faiss
+import math
+import numpy as np
+import pymongo.collection
+
+from huggingface import Embedding, Deberta
+from providers import Sanitize, AnomalyDetection
+from collections import Counter
 
 nltk.download("punkt")
-
-
-class StoreType(str, Enum):
-    FAISS = "FAISS"
-    ATLAS = "ATLAS"
-
-
-class VectorStoreType(BaseModel):
-    type: StoreType
-    address: str | None = None
-
 
 class Guardrail:
     def __init__(
         self,
         model: str | None,
         langfuse: str | None,
-        vector_store: VectorStoreType,
+        vector_store: pymongo.collection.Collection | faiss.Index ,
         similarity_upper_bound: float = 0.8,
         anomaly_upper_bound: float = 0.8,
         entropy_upper_bound: float = 0.8,
     ):
-        if vector_store is None:
-            raise ValueError("A vector is required")
-
         self.model = model
         self.langfuse = langfuse
-        self.vector_store = VectorStore.config_vector_store(vector_store)
-
+        self.vector_store = vector_store
         self.similarity_upper_bound = similarity_upper_bound
         self.anomaly_upper_bound = anomaly_upper_bound
         self.entropy_upper_bound = entropy_upper_bound
@@ -52,13 +34,12 @@ class Guardrail:
         if Sanitize.contains_invisible_characters(query):
             return {"blocked": True, "reason": "invisible characters"}
         
-        query_embedding = Embedding.transform(query)
         with concurrent.futures.ThreadPoolExecutor() as executor:
             future_malicious_similarity = executor.submit(
-                self.query_malicious_similarity, query_embedding
+                self.query_malicious_similarity, query
             )
             future_anomaly = executor.submit(
-                self.query_anomaly_detection, query_embedding
+                self.query_anomaly_detection, query
             )
             future_entropy = executor.submit(
                 self.query_entropy, query
@@ -73,7 +54,7 @@ class Guardrail:
             return {"blocked": True, "reason": "compound score above threshold"}
 
         validation_model_prediction = self.invoke_validation_model(query)
-        if validation_model_prediction.get("prediction") == "malicious":
+        if validation_model_prediction.get("prediction") == "INJECTION":
             return {"blocked": True, "reason": "validation model block"}
 
         return {"blocked": False, "reason": None}
@@ -104,16 +85,43 @@ class Guardrail:
 
         return result, anomaly_score
 
-    def query_malicious_similarity(self, query_embedding):
-        similar_embedding = self.vector_store.find_similar(query_embedding)
-        return similar_embedding
+    def query_malicious_similarity(self, query: str) -> float:
+        embedding = Embedding.transform(query)
+        if isinstance(self.vector_store, pymongo.collection.Collection):
+            pipeline = [
+                {
+                    "$vectorSearch": {
+                        "index": "vector_index",
+                        "queryVector": embedding,
+                        "path": "embedding",
+                        "exact": True,
+                        "limit": 1
+                    }
+                }, {
+                    "$project": {
+                        "_id": 0,
+                        "text": 1,
+                        "score": {
+                        "$meta": "vectorSearchScore"
+                        }
+                    }
+                }
+            ]
+            result = list(self.vector_store.aggregate(pipeline))
+            return result[0]["score"] if result else 0.0
+        else:
+            _, indices = self.vector_store.search(np.array([embedding]), 1)
+            return indices[0][0] if indices else 0.0
 
     def invoke_validation_model(self, query):
-        return {"prediction": "malicious"}
-
+        classified_text = Deberta.classify_text(query)
+        text = classified_text[0]["label"]
+        score = classified_text[0]["score"]
+        
+        logging.info(f"Validation Model Prediction: {text}")
+        logging.info(f"Validation Model Score: {score}")
+        
+        return {"prediction": text, "score": score} 
+        
     def compute_score(self, malicious_similarity, anomaly, entropy):
-        weights = np.array([3.0, 2.0, 1.0])
-        values = np.array([malicious_similarity, anomaly, entropy]) * weights
-        exp_values = np.exp(values - np.max(values))
-        softmax_scores = exp_values / np.sum(exp_values)
-        return np.sum(softmax_scores)
+        return (malicious_similarity + anomaly + entropy) / 3
