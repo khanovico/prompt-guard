4083ee02dbb237a136a2a94000411c9425e9d20e
Khanovico <khanovicdev@gmail.com>
2025-02-16 02:56:44 -0300

    feat: add healthcheck script and update DeBERTa model references

--- huggingface/Deberta.py
+++ huggingface/Deberta.py
@@ -1,14 +1,12 @@
 from transformers import AutoTokenizer, AutoModelForSequenceClassification, pipeline
 import torch
 
+TOKENIZER = "ProtectAI/deberta-v3-base-prompt-injection-v2"
+MODEL = "ProtectAI/deberta-v3-base-prompt-injection-v2"
 
 def classify_text(query: str):
-    tokenizer = AutoTokenizer.from_pretrained(
-        "ProtectAI/deberta-v3-base-prompt-injection-v2"
-    )
-    model = AutoModelForSequenceClassification.from_pretrained(
-        "ProtectAI/deberta-v3-base-prompt-injection-v2"
-    )
+    tokenizer = AutoTokenizer.from_pretrained(TOKENIZER) 
+    model = AutoModelForSequenceClassification.from_pretrained(MODEL)
 
     classifier = pipeline(
         "text-classification",
