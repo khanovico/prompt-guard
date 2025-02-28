9f757066e32a0225a85afdc4fd0faad21a3f4f4d
Khanovico <khanovicdev@gmail.com>
2025-02-28 16:53:03 -0300

    chore: formating Guardrail class

--- guardrail.py
+++ guardrail.py
@@ -12,11 +12,13 @@ from providers import Sanitize, AnomalyDetection
 from collections import Counter
 
 nltk.download("punkt")
-nltk.download('punkt_tab')
+nltk.download("punkt_tab")
+
+
 class Guardrail:
     def __init__(
         self,
-        vector_store: pymongo.collection.Collection | faiss.Index ,
+        vector_store: pymongo.collection.Collection | faiss.Index,
         similarity_upper_bound: float = 0.8,
         anomaly_upper_bound: float = 0.8,
         entropy_upper_bound: float = 0.8,
@@ -29,19 +31,19 @@ class Guardrail:
     def should_block(self, query) -> dict[str, bool | str | None]:
         if Sanitize.contains_invisible_characters(query):
             return {"blocked": True, "reason": "invisible characters"}
-        
+
         malicious_similarity = self.query_malicious_similarity(query)
         if malicious_similarity > self.similarity_upper_bound:
             return {"blocked": True, "reason": "malicious similarity above threshold"}
-         
+
         anomaly, anomaly_score = self.query_anomaly_detection(query)
         if anomaly == "Anomaly" and abs(anomaly_score) < self.anomaly_upper_bound:
             return {"blocked": True, "reason": "anomaly score above threshold"}
-            
+
         entropy_score = self.query_entropy(query)
         if entropy_score > self.entropy_upper_bound:
             return {"blocked": True, "reason": "entropy score above threshold"}
-        
+
         validation_model_prediction = self.invoke_validation_model(query)
         if validation_model_prediction.get("prediction") == "INJECTION":
             return {"blocked": True, "reason": "validation model block"}
@@ -72,7 +74,7 @@ class Guardrail:
         logging.info(f"Prediction: {result}")
         logging.info(f"Anomaly Score: {anomaly_score}")
 
-        return result, abs(anomaly_score) ##  Quanto menor, mais anômalo
+        return result, abs(anomaly_score)  ##  Quanto menor, mais anômalo
 
     def query_malicious_similarity(self, query: str) -> float:
         embedding = Embedding.transform(query)
@@ -84,17 +86,16 @@ class Guardrail:
                         "queryVector": embedding.tolist(),
                         "path": "embedding",
                         "numCandidates": 5,
-                        "limit": 5
+                        "limit": 5,
                     }
-                }, {
+                },
+                {
                     "$project": {
                         "_id": 0,
                         "text": 1,
-                        "score": {
-                            "$meta": "vectorSearchScore"
-                        }
+                        "score": {"$meta": "vectorSearchScore"},
                     }
-                }
+                },
             ]
             result = list(self.vector_store.aggregate(pipeline))
             return result[0]["score"] if result else 0.0
@@ -106,11 +107,11 @@ class Guardrail:
         classified_text = Deberta.classify_text(query)
         text = classified_text[0]["label"]
         score = classified_text[0]["score"]
-        
+
         logging.info(f"Validation Model Prediction: {text}")
         logging.info(f"Validation Model Score: {score}")
-        
-        return {"prediction": text, "score": score} 
-        
+
+        return {"prediction": text, "score": score}
+
     def compute_score(self, malicious_similarity, anomaly, entropy):
         return (malicious_similarity + anomaly + entropy) / 3
