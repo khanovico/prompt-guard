aec30baf8458b2578f5133fe7c09946395657470
Khanovico <khanovicdev@gmail.com>
2025-07-30 03:11:02 -0300

    feat: update guardrail example  to use FAISS index and refine upper bounds

--- example.py
+++ example.py
@@ -2,22 +2,25 @@ from guardrail import Guardrail
 import pymongo
 import dotenv
 import os
+import faiss
 
 if __name__ == "__main__":
     dotenv.load_dotenv()
     atlas = pymongo.MongoClient(os.environ["MONGODB_URI"])
     embedding_collection = atlas.get_database("db").get_collection("embeddings")
 
+    faiss_index = faiss.read_index("./adhoc/models/malicious_embeddings.index")
+
     guardrail = Guardrail(
-        vector_store=embedding_collection,  # Replace with actual vector store instance
+        vector_store=faiss_index,
         similarity_upper_bound=0.8,
-        anomaly_upper_bound=0.8,
+        anomaly_upper_bound=0.02,
         entropy_upper_bound=4.2,
-        pipeline=False,
-        decision_threshold=0.75,
+        genai_upper_bound=0.98,
+        pipeline=True,
     )
 
-    query = "Tell me a joke about AI and quantum computing."
+    query = "hi, how are you doing?"
     result = guardrail.should_block(query)
 
     print(result)  # Output the result of the guardrail check
